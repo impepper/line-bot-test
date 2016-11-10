3481b8f212fc44ff5e4abf975f43e7c3257fdd46
ChiHong Lin <impepper@gmail.com>
2016-11-10 17:30:43 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -11,7 +11,13 @@
   <?php
 $chUser = curl_init();
 curl_setopt($chUser,CURLOPT_URL,"https://api.line.me/v2/bot/profile/U1caf201451c3425c1fd1576ad7ab8c48");
-curl_setopt($chUser, CURLOPT_HTTPHEADER, "Authorization: Bearer FOEEtX4oeGnLbqAMWXQuv+WFCtvBtYUC312zQNFH80BFUcMrjO2nJ5z4ZsAsuz0rwgNbfj+XypPfHuE76plAFmzVLnp8OZbCd0bMB9B5+Nb8MxOS+xLo1BllVRQnNu9mc/SMTOxbnCb2hN265hyj7QdB04t89/1O/w1cDnyilFU=");
+//curl_setopt($chUser, CURLOPT_HTTPHEADER, "Authorization: Bearer FOEEtX4oeGnLbqAMWXQuv+WFCtvBtYUC312zQNFH80BFUcMrjO2nJ5z4ZsAsuz0rwgNbfj+XypPfHuE76plAFmzVLnp8OZbCd0bMB9B5+Nb8MxOS+xLo1BllVRQnNu9mc/SMTOxbnCb2hN265hyj7QdB04t89/1O/w1cDnyilFU=");
+curl_setopt($chUser, CURLOPT_HTTPHEADER,array(
+    'Content-Type: application/json; charser=UTF-8',
+    'Authorization: Bearer FOEEtX4oeGnLbqAMWXQuv+WFCtvBtYUC312zQNFH80BFUcMrjO2nJ5z4ZsAsuz0rwgNbfj+XypPfHuE76plAFmzVLnp8OZbCd0bMB9B5+Nb8MxOS+xLo1BllVRQnNu9mc/SMTOxbnCb2hN265hyj7QdB04t89/1O/w1cDnyilFU='
+    ));
+  
+  
 curl_setopt($chUser, CURLOPT_RETURNTRANSFER, true);
 $resultUser = curl_exec($chUser);
 curl_close($chUser);
