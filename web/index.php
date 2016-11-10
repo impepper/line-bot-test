b8f5a84f15e16d73e05224e6cc2968aac9729f1f
ChiHong Lin <impepper@gmail.com>
2016-11-10 19:11:05 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -9,21 +9,21 @@
 こちらはLINE Messaging APIのデモサイトです。
 
   <?php
-$chUser = curl_init();
-curl_setopt($chUser,CURLOPT_URL,"https://api.line.me/v2/bot/profile/U1caf201451c3425c1fd1576ad7ab8c48");
+//$chUser = curl_init();
+//curl_setopt($chUser,CURLOPT_URL,"https://api.line.me/v2/bot/profile/U1caf201451c3425c1fd1576ad7ab8c48");
 //curl_setopt($chUser, CURLOPT_HTTPHEADER, "Authorization: Bearer FOEEtX4oeGnLbqAMWXQuv+WFCtvBtYUC312zQNFH80BFUcMrjO2nJ5z4ZsAsuz0rwgNbfj+XypPfHuE76plAFmzVLnp8OZbCd0bMB9B5+Nb8MxOS+xLo1BllVRQnNu9mc/SMTOxbnCb2hN265hyj7QdB04t89/1O/w1cDnyilFU=");
-curl_setopt($chUser, CURLOPT_HTTPHEADER,array(
+//curl_setopt($chUser, CURLOPT_HTTPHEADER,array(
     //'Content-Type: application/json; charser=UTF-8',
-    "Authorization: Bearer FOEEtX4oeGnLbqAMWXQuv+WFCtvBtYUC312zQNFH80BFUcMrjO2nJ5z4ZsAsuz0rwgNbfj+XypPfHuE76plAFmzVLnp8OZbCd0bMB9B5+Nb8MxOS+xLo1BllVRQnNu9mc/SMTOxbnCb2hN265hyj7QdB04t89/1O/w1cDnyilFU="
-    ));
-curl_setopt($chUser, CURLOPT_RETURNTRANSFER, true);
-$resultUser = curl_exec($chUser);
-curl_close($chUser);
+//    "Authorization: Bearer FOEEtX4oeGnLbqAMWXQuv+WFCtvBtYUC312zQNFH80BFUcMrjO2nJ5z4ZsAsuz0rwgNbfj+XypPfHuE76plAFmzVLnp8OZbCd0bMB9B5+Nb8MxOS+xLo1BllVRQnNu9mc/SMTOxbnCb2hN265hyj7QdB04t89/1O/w1cDnyilFU="
+//    ));
+//curl_setopt($chUser, CURLOPT_RETURNTRANSFER, true);
+//$resultUser = curl_exec($chUser);
+//curl_close($chUser);
 
-$jsonObj2 = json_decode($resultUser);
-$sourceUserName = $jsonObj2 ->{"displayName"};
-echo "/n Result- "$resultUser."/n";
-echo $sourceUserName;
+//$jsonObj2 = json_decode($resultUser);
+//$sourceUserName = $jsonObj2 ->{"displayName"};
+//echo "/n Result- "$resultUser."/n";
+//echo $sourceUserName;
   
 //$post_data = [
 //	"to" => "U1caf201451c3425c1fd1576ad7ab8c48",
