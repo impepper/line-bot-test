0261d86a604633d4658bd32d5814270702c50d6f
ChiHong Lin <impepper@gmail.com>
2016-11-10 17:33:50 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -13,19 +13,17 @@ $chUser = curl_init();
 curl_setopt($chUser,CURLOPT_URL,"https://api.line.me/v2/bot/profile/U1caf201451c3425c1fd1576ad7ab8c48");
 //curl_setopt($chUser, CURLOPT_HTTPHEADER, "Authorization: Bearer FOEEtX4oeGnLbqAMWXQuv+WFCtvBtYUC312zQNFH80BFUcMrjO2nJ5z4ZsAsuz0rwgNbfj+XypPfHuE76plAFmzVLnp8OZbCd0bMB9B5+Nb8MxOS+xLo1BllVRQnNu9mc/SMTOxbnCb2hN265hyj7QdB04t89/1O/w1cDnyilFU=");
 curl_setopt($chUser, CURLOPT_HTTPHEADER,array(
-    'Content-Type: application/json; charser=UTF-8',
-    'Authorization: Bearer FOEEtX4oeGnLbqAMWXQuv+WFCtvBtYUC312zQNFH80BFUcMrjO2nJ5z4ZsAsuz0rwgNbfj+XypPfHuE76plAFmzVLnp8OZbCd0bMB9B5+Nb8MxOS+xLo1BllVRQnNu9mc/SMTOxbnCb2hN265hyj7QdB04t89/1O/w1cDnyilFU='
+    //'Content-Type: application/json; charser=UTF-8',
+    "Authorization: Bearer FOEEtX4oeGnLbqAMWXQuv+WFCtvBtYUC312zQNFH80BFUcMrjO2nJ5z4ZsAsuz0rwgNbfj+XypPfHuE76plAFmzVLnp8OZbCd0bMB9B5+Nb8MxOS+xLo1BllVRQnNu9mc/SMTOxbnCb2hN265hyj7QdB04t89/1O/w1cDnyilFU="
     ));
-  
-  
 curl_setopt($chUser, CURLOPT_RETURNTRANSFER, true);
 $resultUser = curl_exec($chUser);
 curl_close($chUser);
 
 $jsonObj2 = json_decode($resultUser);
 $sourceUserName = $jsonObj2 ->{"displayName"};
-echo $resultUser;
- echo $sourceUserName;
+echo "/n Result- "$resultUser."/n";
+echo $sourceUserName;
 ?>
 </body>
 </html>
