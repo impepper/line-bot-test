0fb620804bb5f7382b6b6b0f600d471887d8fba2
ChiHong Lin <impepper@gmail.com>
2016-11-10 19:04:39 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -36,19 +36,19 @@ $post_data = [
 		"text":"Hello, world2"
 	}]
 ];
-$ch = curl_init();
-curl_setopt($ch,CURLOPT_URL,"https://api.line.me/v2/bot/message/push");
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    "Content-Type: application/json; charser=UTF-8",
-    "Authorization: Bearer N48DY/N89s/hQfZZTy0hnx4HSEC1yvpfJubFiQfLWcgk+M56cMNgUHz/c4RdqipD986zTvXWYBFU3wdCHauRRkRdmlOB1auzWe0sz9PGMWDGMGJ1ah0ApLAipNyACy5EwAIiWZt6pU/Yxk6h8HhVpAdB04t89/1O/w1cDnyilFU="
-    ));
-$result2 = curl_exec($ch);
-curl_close($ch);  
+//$ch = curl_init();
+//curl_setopt($ch,CURLOPT_URL,"https://api.line.me/v2/bot/message/push");
+//curl_setopt($ch, CURLOPT_POST, true);
+//curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+//curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+//    "Content-Type: application/json; charser=UTF-8",
+//    "Authorization: Bearer N48DY/N89s/hQfZZTy0hnx4HSEC1yvpfJubFiQfLWcgk+M56cMNgUHz/c4RdqipD986zTvXWYBFU3wdCHauRRkRdmlOB1auzWe0sz9PGMWDGMGJ1ah0ApLAipNyACy5EwAIiWZt6pU/Yxk6h8HhVpAdB04t89/1O/w1cDnyilFU="
+//    ));
+//$result2 = curl_exec($ch);
+//curl_close($ch);  
   
-?>
+
 </body>
 </html>
