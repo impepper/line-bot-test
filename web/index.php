e3d7ae4f75a9efcc9aa33334683c1394ec4d20c5
ChiHong Lin <impepper@gmail.com>
2016-11-10 20:42:23 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -25,24 +25,21 @@ $sourceUserName = $jsonObj2 ->{"displayName"};
 echo "\n Result- ".$resultUser."\n";
 echo $sourceUserName;
   
-//$post_data = {
-//	"to" => "U1caf201451c3425c1fd1576ad7ab8c48",
-//	"messages" => [{
-//		"type":"text",
-//		"text":"Hello, world1"
-//	},
-//	{
-//		"type":"text",
-//		"text":"Hello, world2"
-//	}]
-//};
+$post_data = [
+	'to' => 'U1caf201451c3425c1fd1576ad7ab8c48',
+	'messages' => [[
+		'type':'text',
+		'text':'Hello, world1'
+	],]
+];
 $ch = curl_init();
 curl_setopt($ch,CURLOPT_URL,"https://api.line.me/v2/bot/message/push");
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array('to'=>'U1caf201451c3425c1fd1576ad7ab8c48','messages'=>[['type'=>'text','text'=>'hello world'],])));
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+//curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array('to'=>'U1caf201451c3425c1fd1576ad7ab8c48','messages'=>[['type'=>'text','text'=>'hello world'],])));
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     "Content-Type: application/json; charser=UTF-8",
     "Authorization: Bearer N48DY/N89s/hQfZZTy0hnx4HSEC1yvpfJubFiQfLWcgk+M56cMNgUHz/c4RdqipD986zTvXWYBFU3wdCHauRRkRdmlOB1auzWe0sz9PGMWDGMGJ1ah0ApLAipNyACy5EwAIiWZt6pU/Yxk6h8HhVpAdB04t89/1O/w1cDnyilFU="
     ));
